f728fb189e1d74ce5f484b2c2e3a76a7ddf339a4
igor <ftain44@mail.ru>
2023-02-16 18:06:11 +0300

    Теперь начал все понимать

---
+++ Sem2Task11/Program.cs
@@ -0,0 +1,13 @@
+﻿// Задача №11
+// Напишите программу, которая выводит случайное 
+// трёхзначное число и удаляет вторую цифру этого числа.
+
+Random rnd = new Random();
+
+int rndNum = rnd.Next(100, 1000);
+Console.WriteLine($"Рандомное число = {rndNum}");
+
+int lastDigit = rndNum % 10;
+int firstDigit = rndNum / 100;
+rndNum = firstDigit * 10 + lastDigit;
+Console.WriteLine($"Получившееся число = {rndNum}");
---
+++ Sem2Task9/Program.cs
@@ -0,0 +1,21 @@
+﻿
+System.Random numSintezator = new System.Random();
+//Вариант 1
+int rndNumber = numSintezator.Next(10,100);
+Console.WriteLine(rndNumber);
+int firstNum = rndNumber/10;
+int secondNum = rndNumber%10;
+
+if(firstNum>secondNum)
+{
+    Console.WriteLine(firstNum);
+}
+else
+{
+    Console.WriteLine(secondNum);
+}
+//Вариант 2
+char[] digits = numSintezator.Next(10,100).ToString().ToCharArray();
+int firstNum = ((int)digits[0])-48;
+int secondNum = ((int)digits[1])-48;
+Console.WriteLine(digits);
