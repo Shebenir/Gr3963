a963a07474b6c75e0116a0196bbdf4644d983f36
igor <ftain44@mail.ru>
2023-02-25 19:27:35 +0300

    Новый день новый урок

---
+++ Sem5Task31/Program.cs
@@ -0,0 +1,61 @@
+﻿//Задайте массив из 12 элементов, заполненный случайными числами из промежутка [-9, 9]. Найдите сумму отрицательных и положительных элементов массива.
+int[] Gen1DArray(int len, int minValue, int maxValue)
+{
+    //Блок корректировки входных данных
+    if (minValue > maxValue)
+    {
+        int buf = minValue;
+        minValue = maxValue;
+        maxValue = buf;
+    }
+
+    Random rnd = new Random();
+    int[] arr = new int[len];
+    for (int i = 0; i < arr.Lenght; i++)
+    {
+        arr[i] = rnd.Next(minValue, maxValue + 1);
+    }
+    return arr;
+}
+
+//Печатаем одномерный массив 
+void Print1DArray(int[] arr)
+{
+    Console.WriteLine("[");
+    for (int i = 0; i < arr.Length - 1; i++)
+    {
+        Console.Write(arr[i] + ", ");
+    }
+    Console.WriteLine(arr[arr.Length - 1] + "]");
+}
+
+void PrintData(string res, int value)
+{
+    Console.WriteLine(res + value);
+}
+
+(int posit, int negot) NegPosSum(int[] arr)
+{
+    int positSum = 0;
+    int negatSum = 0;
+
+    for (int i = 0; i < arr.Length; i++)
+    {
+        if (arr[i] > 0)
+        {
+            positSum += arr[i];
+        }
+        else
+        {
+            negatSum += arr[i];
+        }
+    }
+
+    return (positSum, negatSum);
+}
+
+int[] tesrArr = Gen1DArray(12,-9,9);
+Print1DArray(testArr);
+(int posit,int negot) results = NegPosSum(arr);
+PrintData("Сумма положительных чисел в массиве: ", results.posit);
+PrintData("Сумма отрицательных чисел в массиве: ", results.negot);
---
+++ Sem5Task33/Program.cs
@@ -0,0 +1,46 @@
+﻿//Задайте массив. Напишите программу, которая определяет, присутствует ли заданное число в массиве.
+int[] Gen1DArray(int len, int min, int max)
+{
+    // Блок корректировки входных данных 
+    if(min > max)
+    {
+        int buf = min;
+        min = max;
+        max = buf;
+    }
+    int[] arr = new int[len];
+    for(int i = 0; i < len; i++)
+    {
+        arr[i] = new Random().Next(min, max + 1);
+    }
+    return arr;
+}
+void Print1DArray(int[]arr)
+{
+    Console.Write("[");
+    for(int i = 0; i < arr.Length-1; i++)
+    {
+        Console.Write(arr[i]+", ");    
+    }
+    Console.Write(arr[arr.Length-1]);
+    Console.WriteLine("]");
+}
+
+int SearchElem(int[] arr, int elem)
+{
+    int result = - 1;
+    for(int i = 0; i < arr.Length; i++)
+    {
+        if(arr[i] == elem)
+        {
+            result = i; break;
+        }
+    }
+    return result; 
+}
+int[] testArr = Gen1DArray(12, -9, 9);
+Print1DArray(testArr);
+Console.Write("Введите искомый элемент: ");
+int elem = int.Parse(Console.ReadLine());
+int res = SearchElem(testArr, elem);
+Console.WriteLine("Искомый эелемент находится в массиве с индексом: "+res);
